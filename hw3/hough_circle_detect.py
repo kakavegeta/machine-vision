7671fa075d0cd854220ce293457dc40fcfbbf19a
vegeka <zhj199305@gmail.com>
2020-03-23 20:15:46 -0400

    hough circle

--- hw3/hough_circle_detect.py
+++ hw3/hough_circle_detect.py
@@ -1,38 +1,6 @@
 import cv2 as cv
 import numpy as np
 
-def hough_circle(edges, r_min, r_max, threshold,region):
-    M, N = edges.shape
-    print(M,N)
-    cv.imwrite("output/edge.png", edges)
-    # extract edges coordinate
-    edges = np.argwhere(edges[:,:])
-    # print(type(edges), edges.shape)
-
-    # accumulator matrix A[a, b, r]
-    A = np.zeros((M+2*r_max, N+2*r_max, r_max))
-    B = np.zeros((M+2*r_max, N+2*r_max, r_max))
-
-    # pre calculate theta
-    theta = np.arange(0,360)*np.pi/180
-
-    #voting
-    for r in range(r_min, r_max, 1):
-        m, n = r+1, r+1
-        bprint = np.zeros((2*m, 2*n))
-        for t in theta:
-            i = int(np.round(r*np.cos(t)))
-            j = int(np.round(r*np.sin(t)))
-            bprint[m+i, n+j] = 1
-        constant = np.argwhere(bprint).shape[0]
-        for x,y in edges:
-            A[x-m+r_max:x+m+r_max, y-n+r_max:y+n+r_max, r] += bprint
-        A[r][A[r]<threshold*constant/r] = 0
-        
-
-    return B[r_max:-r_max, r_max:-r_max, :]
-
-
 def hough_circle_foo(edges, r_min, r_max, threshold, region):
     M, N = edges.shape
     x_min, x_max, y_min, y_max = region
@@ -69,7 +37,7 @@ if __name__ == "__main__":
     cv.imwrite("output/edges.png", edges)
     np.savetxt('output/edges.txt', edges)
      
-    circles = hough_circle_foo(edges, 30, 51, 0.8, (120,230,160,270)) 
+    circles = hough_circle_foo(edges, 25, 51, 0.7, (120,230,160,270)) 
     np.save("output/circles", circles)
 
     for a, b, r in circles:
